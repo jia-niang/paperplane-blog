3307a4bc072846d4a7d5fdfeff3934efa5e5c7b3
jia-niang <1@paperplane.cc>
2024-12-20 08:13:12 +0800

    feat(blog): 更新短链生成方式，现在会在编译阶段请求 paperplane-api 的短网址系统

---
+++ scripts/hexo-short-url/hexo-short-url.js
@@ -0,0 +1,30 @@
+async function generateShortUrl(post) {
+  if (
+    !process.env.PAPAERPLANE_API_ADMIN_HEADER_NAME ||
+    !process.env.PAPAERPLANE_API_ADMIN_HEADER_VALUE
+  ) {
+    return
+  }
+
+  const shortUrl = await require('axios')
+    .post(
+      'https://app.paperplane.cc/api/shorts',
+      {
+        url: post.permalink,
+        type: 'SYSTEM',
+        expiredAt: null,
+      },
+      {
+        headers: {
+          [process.env.PAPAERPLANE_API_ADMIN_HEADER_NAME]:
+            process.env.PAPAERPLANE_API_ADMIN_HEADER_VALUE,
+        },
+      }
+    )
+    .then(res => res.data)
+    .then(data => data.p01ShortUrl)
+
+  post.shortUrl = shortUrl
+}
+
+module.exports = generateShortUrl
