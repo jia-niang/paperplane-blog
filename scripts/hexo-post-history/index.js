ed4102c18f53a245cff47066f8831fa9f339d6a2
jia-niang <1@paperplane.cc>
2025-02-26 10:12:21 +0800

    feat(hexo-plugin): 更新插件代码组织方式；现在不会在本地开发时显示修订记录

--- scripts/hexo-post-history/index.js
+++ scripts/hexo-post-history/index.js
@@ -1,1 +1,34 @@
-hexo.extend.filter.register('before_post_render', require('./hexo-post-history'))
+async function generateHistory(post) {
+  if ((hexo.env.cmd || "").includes("server")) {
+    return;
+  }
+
+  if (post.history === 0) {
+    return;
+  }
+
+  const filePath = `source/_posts/${encodeURIComponent(post.slug)}.md`;
+  const metaData = await require("axios")
+    .get(
+      `https://git.paperplane.cc/api/v1/repos/jia-niang/paperplane-blog/commits?sha=master&path=${filePath}&stat=false&verification=false`
+    )
+    .then((res) => res.data)
+    .catch(() => []);
+  const history = metaData.map((item) => {
+    const ts = new Date(item.created).valueOf();
+    return {
+      message: item.commit.message,
+      url: item.html_url,
+      ts,
+    };
+  });
+
+  const result = {
+    url: `https://git.paperplane.cc/jia-niang/paperplane-blog/commits/branch/master/${filePath}`,
+    list: history,
+  };
+
+  post.historyMeta = result;
+}
+
+hexo.extend.filter.register("before_post_render", generateHistory);
--- scripts/hexo-short-url/index.js
+++ scripts/hexo-short-url/index.js
@@ -1,1 +1,29 @@
-hexo.extend.filter.register('before_post_render', require('./hexo-short-url'))
+async function generateShortUrl(post) {
+  if (
+    !process.env.PAPAERPLANE_API_ADMIN_HEADER_NAME ||
+    !process.env.PAPAERPLANE_API_ADMIN_HEADER_VALUE
+  ) {
+    return;
+  }
+
+  const shortUrl = await require("axios")
+    .post(
+      "https://console.paperplane.cc/api/shorts",
+      {
+        url: post.permalink,
+        type: "SYSTEM",
+        expiredAt: null,
+      },
+      {
+        headers: {
+          [process.env.PAPAERPLANE_API_ADMIN_HEADER_NAME]:
+            process.env.PAPAERPLANE_API_ADMIN_HEADER_VALUE,
+        },
+      }
+    )
+    .then((res) => res.data.data.p01ShortUrl);
+
+  post.shortUrl = shortUrl;
+}
+
+hexo.extend.filter.register("before_post_render", generateShortUrl);
