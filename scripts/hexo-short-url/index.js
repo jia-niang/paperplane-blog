350ad6441a7ef325d1d6b803f17b73d5ba2eca96
jia-niang <1@paperplane.cc>
2026-01-12 01:36:02 +0800

    fix(scripts): 更新短链接生成

--- scripts/hexo-short-url/index.js
+++ scripts/hexo-short-url/index.js
@@ -6,18 +6,11 @@ async function generateShortUrl(post) {
   const shortUrl = await require("axios")
     .post(
       "https://paperplane.cc/api/short",
-      {
-        url: post.permalink,
-        public: true,
-        reuse: true,
-      },
-      {
-        headers: {
-          "X-API-KEY": process.env.API_KEY,
-        },
-      }
+      { url: post.permalink, public: true, reuse: true },
+      { headers: { "X-API-KEY": process.env.API_KEY } }
     )
-    .then((res) => res.data.data.p01ShortUrl);
+    .then((res) => res.data)
+    .then((res) => "p01.cc/s/" + res.key);
 
   post.shortUrl = shortUrl;
 }
